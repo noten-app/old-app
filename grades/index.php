1db35389123c71beb2298f9bd7f82c6101179fb3
CuzImBisonratte <cuzimbisonratte@gmail.com>
2023-03-01 18:15:41 +0100

    Add navbar height var + Mobile grade element echoing

---
+++ grades/index.php
@@ -0,0 +1,107 @@
+<?php 
+
+    // Check login state
+    session_start();
+    require("../res/php/checkLogin.php");
+    if(!checkLogin()) header("Location: /account/login");
+
+    // Get config
+    require("../config.php");
+
+    // DB Connection
+    $con = mysqli_connect(
+        config_db_host,
+        config_db_user,
+        config_db_password,
+        config_db_name
+    );
+    if(mysqli_connect_errno()) exit("Error with the Database");
+
+    // Get all classes
+    $classlist = array();
+    if($stmt = $con->prepare("SELECT name, color, id, last_used, average FROM ".config_table_name_classes." WHERE user_id = ?")) {
+        $stmt->bind_param("s", $_SESSION["user_id"]);
+        $stmt->execute();
+        $stmt->bind_result($class_name, $class_color, $class_id, $class_last_used, $class_grade_average);
+        while ($stmt->fetch()) {
+            $classlist[] = array(
+                "name" => $class_name,
+                "color" => $class_color,
+                "id" => $class_id,
+                "last_used" => $class_last_used,
+                "average" => $class_grade_average
+            );
+        }
+        $stmt->close();
+    } else {
+        if(isset($classlist[0])) header("Location: ./?class=".$classlist[0]["id"]);
+    }
+
+    // DB Con close
+    $con->close();
+
+    // var_dump($classlist);
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NotenApp</title>
+    <link rel="icon" type="image/x-icon" href="/res/img/favicon.ico" />
+    <link rel="stylesheet" href="/res/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="/res/fontawesome/css/solid.min.css">
+    <link rel="stylesheet" href="/res/css/fonts.css">
+    <link rel="stylesheet" href="/res/css/main.css">
+    <link rel="stylesheet" href="/res/css/navbar.css">
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <nav>
+        <a href="/" class="nav-link">
+            <div class="navbar_icon">
+                <i class="fas fa-home"></i>
+            </div>
+        </a>
+        <a href="/calendar/" class="nav-link">
+            <div class="navbar_icon">
+                <i class="fas fa-calendar-alt"></i>
+            </div>
+        </a>
+        <a href="/homework/" class="nav-link">
+            <div class="navbar_icon">
+                <i class="fas fa-calendar-check"></i>
+            </div>
+        </a>
+        <a href="/grades/" class="nav-link nav-active">
+            <div class="navbar_icon">
+                <i class="fa-solid fa-graduation-cap"></i>
+            </div>
+        </a>
+        <a href="/classes/" class="nav-link">
+            <div class="navbar_icon">
+                <i class="fas fa-book"></i>
+            </div>
+        </a>
+    </nav>
+    <main id="main">
+        <div class="class_list">
+            <?php 
+            foreach ($classlist as $class) {
+                echo '<div class="class_entry';
+                echo '" onclick="location.assign(\'./?class='.$class["id"].'\')" style="border-color:#'.$class["color"].'">';
+                echo '<div class="class_entry-name">'.$class["name"].'</div>';
+                if($class["average"] != 0) echo '<div class="class_entry-average"> &empty; '.$class["average"].'</div>';
+                echo '</div>';
+            }
+            ?>
+        </div>
+    </main>
+    <script src="/res/js/themes/themes.js"></script>
+</body>
+
+</html>
