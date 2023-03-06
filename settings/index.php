10765bf80b7e619285f85745b43dcdb95366add6
CuzImBisonratte <CuzImBisonratte@gmail.com>
2023-03-06 19:07:52 +0100

    Add settings frontend

--- settings/index.php
+++ settings/index.php
@@ -46,12 +46,150 @@
             </div>
         </a>
         <div class="nav-link">
-            <div class="navbar_icon">
+            <div class="navbar_icon navbar_icon-save">
                 <i class="fa-solid fa-floppy-disk"></i>
             </div>
         </div>
     </nav>
     <main id="main">
+        <div class="group_container" id="account-settings">            
+            <div class="account-greeting">
+                <span id="account_greeting-naa">Naaa,</span>
+                <span id="account_greeting-name"><?=$_SESSION["user_name"]?></span>
+            </div>
+            <div class="account-icon">
+                <i class="fas fa-user"></i>
+            </div>
+        </div>
+        <span class="container-title">Grading</span>
+        <div class="group_container" id="grade-settings">
+            <div class="container_item">
+                <div class="button_divider">
+                    <div class="button_divider-button1 button_divider-button_active">
+                        Grades<br>1,0 - 6,0
+                    </div>
+                    <div class="button_divider-button2">
+                        Points<br>0 - 15
+                    </div>
+                </div>
+            </div>
+            <div class="container_item">
+                Decimals (Rounding)
+                <div class="button_divider">
+                    <div class="button_divider-button_active">
+                        No<br>
+                        2
+                    </div>
+                    <div>
+                        One<br>
+                        1,7
+                    </div>
+                    <div>
+                        Two<br>1,72
+                    </div>
+                </div>
+            </div>
+            <div class="container_item">
+                Show Date
+                <div class="button_divider">
+                    <div class="button_divider-button1 button_divider-button_active">
+                        Yes
+                    </div>
+                    <div class="button_divider-button2">
+                        No
+                    </div>
+                </div>
+            </div>
+        </div>
+        <span class="container-title">Location-based settings</span>
+        <div class="group_container" id="location-settings">
+            <div class="dropdown_container container_item">
+                <div class="dropdown_container-name">
+                    Language
+                </div>
+                <div class="dropdown_container-dropdown_icon">
+                    <i class="fas fa-chevron-down"></i>
+                </div>
+            </div>
+            <div class="dropdown_container container_item">
+                <div class="dropdown_container-name">
+                    Region
+                </div>
+                <div class="dropdown_container-dropdown_icon">
+                    <i class="fas fa-chevron-down"></i>
+                </div>
+            </div>
+            <div class="dropdown_container container_item">
+                <div class="dropdown_container-name">
+                    Off-days
+                </div>
+                <div class="dropdown_container-dropdown_icon">
+                    <i class="fas fa-chevron-down"></i>
+                </div>
+            </div>
+        </div>
+        <span class="container-title">Advanced settings</span>
+        <div class="group_container" id="location-settings">
+            <div class="dropdown_container container_item">
+                <div class="dropdown_container-name">
+                    Export grades
+                </div>
+                <div class="dropdown_container-dropdown_icon">
+                    <i class="fa-solid fa-file-arrow-down"></i>
+                </div>
+            </div>
+            <div class="dropdown_container container_item">
+                <div class="dropdown_container-name">
+                    About this version
+                </div>
+                <div class="dropdown_container-dropdown_icon">
+                    <i class="fas fa-chevron-right"></i>
+                </div>
+            </div>
+        </div>
+        <span class="container-title">More Help</span>
+        <div class="group_container" id="location-settings">
+            <div class="dropdown_container container_item">
+                <div class="dropdown_container-name">
+                    Website
+                </div>
+                <div class="dropdown_container-dropdown_icon">
+                    <i class="fa-solid fa-chevron-right"></i>
+                </div>
+            </div>
+            <div class="dropdown_container container_item">
+                <div class="dropdown_container-name">
+                    Help & FAQ
+                </div>
+                <div class="dropdown_container-dropdown_icon">
+                    <i class="fas fa-chevron-right"></i>
+                </div>
+            </div>
+            <div class="dropdown_container container_item">
+                <div class="dropdown_container-name">
+                    Support
+                </div>
+                <div class="dropdown_container-dropdown_icon">
+                    <i class="fas fa-chevron-right"></i>
+                </div>
+            </div>
+            <div class="dropdown_container container_item">
+                <div class="dropdown_container-name">
+                    Terms of Service
+                </div>
+                <div class="dropdown_container-dropdown_icon">
+                    <i class="fas fa-chevron-right"></i>
+                </div>
+            </div>
+            <div class="dropdown_container container_item">
+                <div class="dropdown_container-name">
+                    Privacy policy
+                </div>
+                <div class="dropdown_container-dropdown_icon">
+                    <i class="fas fa-chevron-right"></i>
+                </div>
+            </div>
+        </div>
     </main>
     <script src="/res/js/themes/themes.js"></script>
 </body>
