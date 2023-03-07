d2a8e2d090655bf4771212ade22e49068c7ca341
CuzImBisonratte <CuzImBisonratte@gmail.com>
2023-03-07 21:12:11 +0100

    Add login script

---
+++ account/login/login.php
@@ -0,0 +1,48 @@
+<?php
+
+    // Start the PHP_session
+    session_start();
+
+    // Variables
+    require('../../config.php');
+
+    // Get input
+    $displayname = $_POST['username'];
+    $username = strtolower($_POST['username']);
+    $password = $_POST['password'];
+
+    // Conect to database
+    $con = mysqli_connect(config_db_host, config_db_user, config_db_password, config_db_name);
+    if (mysqli_connect_errno()) exit("Error connecting to our database! Please try again later."); 
+
+    // Check if account exists
+    $stmt = $con->prepare("SELECT id FROM ".config_table_name_accounts." WHERE username = ?");
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $stmt->store_result();
+    if ($stmt->num_rows == 0) exit("Account $username not found");
+
+    // Get id, salt and password hash from database
+    if ($stmt = $con->prepare("SELECT id, displayname, password, email, account_version FROM ".config_table_name_accounts." WHERE username = ?")) {
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($id, $displayname, $password_hash, $email, $account_version);
+        $stmt->fetch();
+
+        // Add salt and password and check if right
+        if ($account_version != 3) header("Location: https://accounttools.noten-app.de/version_updater/");
+
+        // Check if password is right
+        if (!password_verify($password, $password_hash)) exit("Wrong password"); 
+
+        // Set session variables
+        $_SESSION["login_method"] = "login";
+        $_SESSION["user_name"] = $displayname;
+        $_SESSION["user_id"] = $id;
+        $_SESSION["user_email"] = $email;
+
+        // Redirect to app
+        header("Location: /");
+    }
+?>
