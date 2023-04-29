7a33dc413859d65e6a7185a947e858204bb3587c
Konstantin Protzen <cuzimbisonratte@gmail.com>
2023-04-29 22:32:22 +0200

    Add custom title to settings page

--- settings/index.php
+++ settings/index.php
@@ -28,7 +28,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NotenApp</title>
+    <title>Settings - NotenApp</title>
     <link rel="icon" type="image/x-icon" href="/res/img/favicon.ico" />
     <link rel="stylesheet" href="/res/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="/res/fontawesome/css/solid.min.css">
