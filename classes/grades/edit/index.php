a05fe2300627eeee91e39431245a57635271d598
Konstantin Protzen <cuzimbisonratte@gmail.com>
2023-04-29 22:32:10 +0200

    Add custom title to class views

--- classes/grades/edit/index.php
+++ classes/grades/edit/index.php
@@ -45,7 +45,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NotenApp</title>
+    <title>Edit a grade - <?=$class_name?> - NotenApp</title>
     <link rel="stylesheet" href="/res/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="/res/fontawesome/css/solid.min.css">
     <link rel="stylesheet" href="/res/css/fonts.css">
