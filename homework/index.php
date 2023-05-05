bf868c9b8daa43634d35eafecbb861d82c5378a7
Konstantin Protzen <cuzimbisonratte@gmail.com>
2023-05-05 22:45:19 +0200

    Add show-all button / mechanism

--- homework/index.php
+++ homework/index.php
@@ -1,50 +1,57 @@
-<?php 
+<?php
 
-    // Check login state
-    session_start();
-    require("../res/php/checkLogin.php");
-    if(!checkLogin()) header("Location: /account");
+// Check login state
+session_start();
+require("../res/php/checkLogin.php");
+if (!checkLogin()) header("Location: /account");
 
-    // Get config
-    require("../config.php");
+// Get config
+require("../config.php");
 
-    // DB Connection
-    $con = mysqli_connect(
-        config_db_host,
-        config_db_user,
-        config_db_password,
-        config_db_name
-    );
-    if(mysqli_connect_errno()) exit("Error with the Database");
+// DB Connection
+$con = mysqli_connect(
+    config_db_host,
+    config_db_user,
+    config_db_password,
+    config_db_name
+);
+if (mysqli_connect_errno()) exit("Error with the Database");
 
-    // Get all tasks
-    if($stmt = $con->prepare("SELECT * FROM ".config_table_name_homework." WHERE user_id = ?")) {
-        $stmt->bind_param("i", $_SESSION["user_id"]);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $homework = $result->fetch_all(MYSQLI_ASSOC);
+// Check if all tasks should be shown
+if(isset($_GET["showall"]) && $_GET["showall"] == 1) {
+    $showall_text = "";
+} else {
+    $showall_text = " AND status = 0";
+}
+
+// Get all tasks
+if ($stmt = $con->prepare("SELECT * FROM " . config_table_name_homework . " WHERE user_id = ?".$showall_text)) {
+    $stmt->bind_param("i", $_SESSION["user_id"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $homework = $result->fetch_all(MYSQLI_ASSOC);
+}
+
+// Get all classes
+if ($stmt = $con->prepare("SELECT * FROM " . config_table_name_classes . " WHERE user_id = ?")) {
+    $stmt->bind_param("i", $_SESSION["user_id"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $classes = $result->fetch_all(MYSQLI_ASSOC);
+}
+
+// Order homework by deadline in arrays - arrays by date
+$homework_ordered = array();
+foreach ($homework as $task) {
+    if (!array_key_exists($task["deadline"], $homework_ordered)) {
+        $homework_ordered[$task["deadline"]] = array();
     }
+    $homework_ordered[$task["deadline"]][] = $task;
+}
+ksort($homework_ordered);
 
-    // Get all classes
-    if($stmt = $con->prepare("SELECT * FROM ".config_table_name_classes." WHERE user_id = ?")) {
-        $stmt->bind_param("i", $_SESSION["user_id"]);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $classes = $result->fetch_all(MYSQLI_ASSOC);
-    }
-
-    // Order homework by deadline in arrays - arrays by date
-    $homework_ordered = array();
-    foreach($homework as $task) {
-        if(!array_key_exists($task["deadline"], $homework_ordered)) {
-            $homework_ordered[$task["deadline"]] = array();
-        }
-        $homework_ordered[$task["deadline"]][] = $task;
-    }
-    ksort($homework_ordered);
-
-    // DB Con close
-    $con->close();
+// DB Con close
+$con->close();
 ?>
 
 <!DOCTYPE html>
@@ -83,19 +90,20 @@
                 <i class="fas fa-home"></i>
             </div>
         </a>
-        <?php if($_SESSION["beta_tester"] == 1){ ?>
-        <a href="/calendar/" class="nav-link">
-            <div class="navbar_icon">
-                <i class="fas fa-calendar-alt"></i>
-            </div>
-        </a>
-        <?php } if($_SESSION["beta_tester"] == 1){ ?>
-        <a href="/homework/" class="nav-link nav-active">
-            <div class="navbar_icon">
-                <i class="fas fa-calendar-check"></i>
-            </div>
-        </a>
-        
+        <?php if ($_SESSION["beta_tester"] == 1) { ?>
+            <a href="/calendar/" class="nav-link">
+                <div class="navbar_icon">
+                    <i class="fas fa-calendar-alt"></i>
+                </div>
+            </a>
+        <?php }
+        if ($_SESSION["beta_tester"] == 1) { ?>
+            <a href="/homework/" class="nav-link nav-active">
+                <div class="navbar_icon">
+                    <i class="fas fa-calendar-check"></i>
+                </div>
+            </a>
+
         <?php } ?>
         <a href="/classes/" class="nav-link">
             <div class="navbar_icon">
@@ -105,7 +113,7 @@
     </nav>
     <main id="main">
         <div class="homework_list">
-            <?php 
+            <?php
             // foreach ($homework as $hw_entry) {
             //     echo '<div class="homework_entry">';
             //     echo '<div class="classname">';
@@ -115,19 +123,20 @@
             //     echo '<div class="dot"><i class="fa-regular fa-circle"></i></div>';
             //     echo '</div>';
             // }
-            foreach($homework_ordered as $hw_dategroup){
+            foreach ($homework_ordered as $hw_dategroup) {
                 echo '<div class="homework_deadline';
-                if(strtotime($hw_dategroup[0]["deadline"]) < strtotime("today")) echo ' homework_deadline_late';
-                if(strtotime($hw_dategroup[0]["deadline"]) == strtotime("today")) echo ' homework_deadline_soon';
+                if (strtotime($hw_dategroup[0]["deadline"]) < strtotime("today")) echo ' homework_deadline_late';
+                if (strtotime($hw_dategroup[0]["deadline"]) == strtotime("today")) echo ' homework_deadline_soon';
                 echo '">';
-                echo '<div class="homework_deadline_date">'.date("d.m - l", strtotime($hw_dategroup[0]["deadline"])).'</div>';
+                echo '<div class="homework_deadline_date">' . date("d.m - l", strtotime($hw_dategroup[0]["deadline"])) . '</div>';
                 echo '<div class="homework_deadline_tasks">';
                 foreach ($hw_dategroup as $hw_entry) {
                     echo '<div class="homework_entry">';
                     echo '<div class="classname">';
                     foreach ($classes as $class) if ($class["id"] == $hw_entry["class"]) echo $class["name"];
-                    echo '</div><div class="task">'.$hw_entry["text"].'</div>';
-                    echo '<div class="dot"><i class="fa-regular fa-circle"></i></div>';
+                    echo '</div><div class="task">' . $hw_entry["text"] . '</div>';
+                    if($hw_entry["status"] == 0) echo '<div class="dot"><i class="fa-regular fa-circle"></i></div>';
+                    else echo '<div class="dot"><i class="fa-regular fa-check-circle"></i></div>';
                     switch ($hw_entry["type"]) {
                         case 'b':
                             echo '<div class="type_badge"><i class="fa-solid fa-book"></i></div>';
@@ -144,6 +153,21 @@
                 echo '</div>';
                 echo '</div>';
             }
+            if(isset($_GET["showall"]) && $_GET["showall"] == 1) {
+            ?>
+            <div class="homework_showall" onclick="location.assign('./?showall=0')">
+                <div class="homework_showall-text">Do not view all Tasks</div>
+                <div class="homework_showall-icon"><i class="fa-solid fa-eye-slash"></i></div>
+            </div>
+            <?php 
+                } else {
+            ?>
+            <div class="homework_showall" onclick="location.assign('./?showall=1')">
+                <div class="homework_showall-text">View all Tasks</div>
+                <div class="homework_showall-icon"><i class="fa-solid fa-eye"></i></div>
+            </div>
+            <?php 
+                }
             ?>
         </div>
         <div class="homework_add" onclick="location.assign('./add/')">
