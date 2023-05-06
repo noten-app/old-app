b356697a81e0fee65080a6eebcfd494ccb663bcc
Konstantin Protzen <cuzimbisonratte@gmail.com>
2023-05-06 16:59:00 +0200

    Changed titles + description

--- settings/index.php
+++ settings/index.php
@@ -28,7 +28,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Settings - NotenApp</title>
+    <title>Settings | Noten-App</title>
     <link rel="icon" type="image/x-icon" href="/res/img/favicon.ico" />
     <link rel="stylesheet" href="/res/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="/res/fontawesome/css/solid.min.css">
